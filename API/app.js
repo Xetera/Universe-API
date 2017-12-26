19695c01a786aaf158d6ea33c475d31e14860365
Ali <mythologyali@gmail.com>
2017-12-26 00:35:25 -0800

    detailed responses

--- API/app.js
+++ API/app.js
@@ -4,21 +4,40 @@ let express = require('express');
 const mysql = require('mysql');
 
 let app = express();
-const db = new Database();
+
 
 class API extends Database {
 
     constructor(){
         super();
         app.set('trust proxy', true);
-
+        this.endpoints =
+            {
+                general: '/',
+                peopleByWorld: '/people/:world'
+            }
     }
 
     connect(){
 
     }
+    logResponse(type, endpoint,destination){
+        let request = ""
+        if (type ===  "get" || "g".toUpperCase()){
+            request = "GET"
+        }
+        else if (type === "post" || "p".toUpperCase()){
+            request = "POST"
+        }
+        console.log(`[Response Sent]: ${request} ${endpoint} to ${destination}`)
+    }
 
+    logNewConnection(ip){
+        console.log(`Got a connection from ${ip}`);
+    }
 }
+
+const api = new API();
 
 app.listen(3030, () => {
     console.log("API Server started at port 3000");
@@ -31,27 +50,31 @@ app.listen(3030, () => {
 
 
 app.get('/', function (req, res) {
-    console.log(`Got a connection from ${req.ip}`);
-    res.sendStatus(200);
+    api.logNewConnection(req.ip);
+
+
+    res.status(200).send("Hey There!");
 });
 
 app.get('/people/:world', function(req,res){
+    api.logNewConnection(req.ip);
+
     let worldType = req.params.world;
-    console.log(worldType);
+
     let prepared = mysql.format(`SELECT * FROM people WHERE world=?`, [worldType]);
     console.log(prepared);
-
-    db.conn.then(function(conn){
+    api.conn.then(function(conn){
         return conn.query(prepared)
             .then(function(rows){
-                console.log("[Response Sent]");
+                api.logResponse("g", api.endpoints.peopleByWorld, req.ip);
                 if (rows.length === 0){
-                    return res.sendStatus(404);
+                    return res.status(404).send(`${worldType} is not a world`);
                 }
                 res.send(rows);
             })
             .catch(function(err){
-                res.sendStatus(404);
+                res.status(404).send(err);
+                console.log(err)
             })
     });
 
